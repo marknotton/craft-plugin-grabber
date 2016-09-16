ddc48930c03b9d34c9c9eb490571a39594b63b39
Mark Notton <mark@marknotton.uk>
2016-09-16 23:47:41 +0100

    Added fallback page strings for error pages

--- grabber/services/Grabber_PageService.php
+++ grabber/services/Grabber_PageService.php
@@ -5,6 +5,10 @@ class Grabber_PageService extends BaseApplicationComponent {
 
   public function page() {
     $page = craft()->grabber_entry->entry()['slug'];
+    $errors = array("400", "403", "404", "500", "503");
+    if (in_array($page, $errors)) {
+      $page = "error-".$page;
+    }
     return !empty($page) ? $page : 'home';
   }
 
